ef77fd25b74a7d9b572a2bcbaeb3f93ee65a5d73
thallium <thallium@users.noreply.github.com>
2022-08-27 04:22:28 +0000

    deploy: d143ef4272a9f1bcb2b68f0b5bf830bfa54913f5

--- js/collapse.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.collapsible__button').forEach(button => {
-  button.addEventListener('click', () => {
-    button.classList.toggle('collapsible__button--active')
-  })
-})
